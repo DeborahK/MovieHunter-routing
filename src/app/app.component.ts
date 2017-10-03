6377e7b4167aaff3cd3dfd3a8d41f2403b3bf431
Deborah Kurata <deborahk@insteptech.com>
2017-10-03 11:38:45 -0700

    Implement spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 @Component({
     selector: 'mh-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
     pageTitle: string = 'InStep Movie Hunter';
+
+    loading: boolean = true;
+    
+        constructor(private router: Router) {
+    
+            router.events.subscribe((routerEvent: Event) => {
+                this.checkRouterEvent(routerEvent);
+            });
+        }
+    
+        checkRouterEvent(routerEvent: Event): void {
+            if (routerEvent instanceof NavigationStart) {
+                this.loading = true;
+            }
+    
+            if (routerEvent instanceof NavigationEnd ||
+                routerEvent instanceof NavigationCancel ||
+                routerEvent instanceof NavigationError) {
+                this.loading = false;
+            }
+        }
 }
